a39bf372f663738d4e94cd5b8823e310cc436bc1
Ben Song <benedict04song@gmail.com>
2024-04-19 01:28:26 -0400

    Added very rudimentary driver, debugged emulator, implemented LDA, TAX, INX, BRK.

--- core/cpu/ucpu.c
+++ core/cpu/ucpu.c
@@ -5,7 +5,53 @@
  * @author Benedict Song <benedict04song@gmail.com>
  */
 #include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
 #include "cpu/ucpu.h"
+
+/*
+ * Mapping from opcode to canonical opcode (NOT FULLY IMPLEMENTED)
+ */
+const addr_mode_t OPCODE_TO_CANONICAL[] = {
+    O_BRK, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x00 - 0x0F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x10 - 0x1F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x20 - 0x2F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x30 - 0x3F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x40 - 0x4F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x50 - 0x5F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x60 - 0x6F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x70 - 0x7F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x80 - 0x8F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x90 - 0x9F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, O_LDA, O_TAX, 0, 0, 0, 0, 0, // 0xA0 - 0xAF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0xB0 - 0xBF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0xC0 - 0xCF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0xD0 - 0xDF
+    0, 0, 0, 0, 0, 0, 0, 0, O_INX, 0, 0, 0, 0, 0, 0, 0, // 0xE0 - 0xEF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  // 0xF0 - 0xFF
+};
+
+/*
+ * Mapping from opcode to addressing mode (NOT FULLY IMPLEMENTED)
+ */
+const uint8_t OPCODE_TO_ADDRMODE[] = {
+    IMPL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x00 - 0x0F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x10 - 0x1F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x20 - 0x2F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x30 - 0x3F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x40 - 0x4F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x50 - 0x5F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x60 - 0x6F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x70 - 0x7F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x80 - 0x8F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x90 - 0x9F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, IMMED, IMPL, 0, 0, 0, 0, 0, // 0xA0 - 0xAF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0xB0 - 0xBF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0xC0 - 0xCF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0xD0 - 0xDF
+    0, 0, 0, 0, 0, 0, 0, 0, IMPL, 0, 0, 0, 0, 0, 0, 0, // 0xE0 - 0xEF
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  // 0xF0 - 0xFF
+};
 
 /**
  * @brief
@@ -22,11 +68,22 @@ static inline uaddr_t pack(byte_t large, byte_t small) {
 }
 
 /**
- * @brief From an emulated memory address, calculates the true pointer in the emulator's VM space.
+ * @brief
  */
-static inline void *true_ptr(ucpu_t *cpu, void *emulated_ptr) {
-    // from cpu pointer offset, add the size of everything *but* the memory array
-    return (void *)(cpu + sizeof(ucpu_t) - UCPU_MEM_CAP*sizeof(byte_t));
+static void set_flag(ucpu_t *cpu, flag_t flag, bool value) {
+    if (value)
+        cpu->status |= (1u << ((int) flag));
+    else
+        cpu->status &= ~(1u << ((int) flag));
+}
+
+/**
+ * @brief
+ *
+ * @returns false if negative, true if positive
+ */
+static bool sign(byte_t u) {
+    return !(u >> (sizeof(byte_t)*8 - 1));
 }
 
 /**
@@ -42,15 +99,24 @@ void init_cpu(ucpu_t *cpu) {
     // zero-initialize memory
     memset(cpu->memory, UNIL, UCPU_MEM_CAP);
     // initialize all registers (placeholders for now)
-    cpu->PC = 0; // program counter
-    cpu->A = 0; // accumulator
-    cpu->X = 0; // generic register X
-    cpu->Y = 0; // generic register Y
-    cpu->S = 0; // stack pointer
-    cpu->status = 0; // status "register"
+    cpu->PC = 0u; // program counter
+    cpu->A = 0u; // accumulator
+    cpu->X = 0u; // generic register X
+    cpu->Y = 0u; // generic register Y
+    cpu->S = 0u; // stack pointer
+    cpu->status = 16u; // status "register" -- bit 5 always set
 }
 
-clk_t step(ucpu_t *cpu, byte_t *program, int program_size) {
+int drive(ucpu_t *cpu, byte_t *program, int program_size) {
+    while (1) {
+        if (cpu->PC >= program_size) { // check for termination this way (for now!)
+            return 0;
+        }
+        step(cpu, program);
+    }
+}
+
+clk_t step(ucpu_t *cpu, byte_t *program) {
     // get the current opcode
     opcode_t op = program[cpu->PC];
     // get the addressing mode
@@ -60,6 +126,7 @@ clk_t step(ucpu_t *cpu, byte_t *program, int program_size) {
     // as well as the (interpreted) operand itself
     byte_t operand = 0;
     switch (addr_mode) {
+        case REL:
         case IMMED: {
             operand = program[cpu->PC + 1]; // assume program has been assembled properly
                                             // and that we can do this safely
@@ -67,60 +134,97 @@ clk_t step(ucpu_t *cpu, byte_t *program, int program_size) {
             break;
         }
         case ZPAGE: {
-            operand = *(byte_t *) true_ptr(cpu,
-                                    program[cpu->PC + 1]);
+            operand = cpu->memory[ program[cpu->PC + 1] ];
             cpu->PC += 2;
             break;
         }
         case ZPAGE_X: {
-            operand = *(byte_t *) true_ptr(cpu,
-                            (program[cpu->PC + 1] + cpu->X) % 256);
+            operand = cpu->memory [
+                        (program[cpu->PC + 1] + cpu->X) % 256
+            ];
+            cpu->PC += 2;
             break;
         }
         case ZPAGE_Y: {
-            operand = *(byte_t *) true_ptr(cpu,
-                            (program[cpu->PC + 1] + cpu->Y) % 256);
+            operand = cpu->memory [
+                        (program[cpu->PC + 1] + cpu->Y) % 256
+            ];
+            cpu->PC += 2;
             break;
         }
+        case INDIR: // literally the same as ABS except bytes are interp. differently
         case ABS: {
-            operand = *(byte_t *) true_ptr(cpu,
-                        pack(program[cpu->PC + 2], program[cpu->PC + 1]); // little endian
+            operand = cpu->memory [
+                        pack(program[cpu->PC + 2], program[cpu->PC + 1]) // little endian
+            ];
+            cpu->PC += 3;
+            break;
+        }
+        case INDIR_X: {
+            // not implemented
+            cpu->PC += 2;
             break;
         }
         case ABS_X: {
-            operand = *(byte_t *) true_ptr(cpu,
+            operand = cpu->memory [
                       (pack(program[cpu->PC + 2], program[cpu->PC + 1])
-                       + cpu->X) % 256);
+                       + cpu->X) % 256
+            ];
+            cpu->PC += 3;
+            break;
+        }
+        case INDIR_Y: {
+            // not implemented
+            cpu->PC += 2;
             break;
         }
         case ABS_Y: {
-            operand = *(byte_t *) true_ptr(cpu,
+            operand = cpu->memory [
                       (pack(program[cpu->PC + 2], program[cpu->PC + 1])
-                       + cpu->Y) % 256);
+                       + cpu->Y) % 256
+            ];
+            cpu->PC += 3;
             break;
         }
-        case REL: {
-            operand = ...;
+        default: {
+            cpu->PC += 1;
         }
     }
     // get the actual operation class
     opcode_t canon = OPCODE_TO_CANONICAL[op];
 
     switch (canon) {
-
-    case O_ADC: {
-
-        byte_t operand = program[cpu->PC + 1]; // note you cannot do this if there might not
-                                               // be an operand (ex. ACCUM case of O_ASL)
-        switch (addr_mode) {
-
-        case IMMED: {
-            // 2 bytes, 2 cycles
+        case O_LDA: {
+            cpu->A = operand;
+            set_flag(cpu, ZERO, cpu->A == 0);
+            set_flag(cpu, NEGATIVE, !sign(cpu->A));
+            break;
+        }
+        case O_TAX: {
+            cpu->X = cpu->A;
+            set_flag(cpu, ZERO, cpu->X == 0);
+            set_flag(cpu, NEGATIVE, !sign(cpu->X));
+            break;
+        }
+        case O_INX: {
+            cpu->X++;
+            set_flag(cpu, ZERO, cpu->X == 0);
+            set_flag(cpu, NEGATIVE, !sign(cpu->X));
+            break;
+        }
+        case O_BRK: {
+            set_flag(cpu, BREAK, true);
+            break;
+        }
+        default: {
 
         }
-
     }
+}
 
-    }
+void dump_cpu(FILE *out, ucpu_t *cpu) {
+     fprintf(out, "PC: %" PRIu16 " A: %" PRIu8 " X: %" PRIu8 " "
+                "Y: %" PRIu8 " S: %" PRIu8 " status: %" PRIu8 "\n",
+                cpu->PC, cpu->A, cpu->X, cpu->Y, cpu->S, cpu->status);
 }
 
