0fbef504642d7041e339c820761f86cdfe870cad
Ben Song <benedict04song@gmail.com>
2024-04-30 13:42:44 -0400

    Finished assembler

--- eval/py/assemble.py
+++ eval/py/assemble.py
@@ -1,35 +1,38 @@
 from typing import TypeAlias, Generator
 from spec import NES_SPEC
-import argparse, re, sys
+import argparse, re, sys, io, os.path
 
 TOK_DELIMITER = ' '
+LINE_DELIMITER = '\n'
 INSTR_DELIMITER = ','
 
 ByteGenerator: TypeAlias = Generator[int, None, None]
 
-parser = argparse.ArgumentParser(
-            description="Assembles a .uasm file into an executable .nes binary.",
-            epilog="Helper script written by me!")
-
-# info message
-parser.add_argument(
-    "-h", "--help", action="help", help="Prints this message."
-)
-
-# verbose mode
-parser.add_argument(
-    "-v", "--verbose", action="store_true", help="Prints a lot of extra information."
-)
-
-# path of .uasm file to assemble
-parser.add_argument(
-    "filepath", help="Path of the .uasm file to assemble."
-)
-
-args = parser.parse_args()
-
-UASM_FILE = args.filepath
-VERBOSE = args.verbose
+def translate(addrmode: str) -> str:
+    # it's just a script, doesn't have to be nice.
+    if addrmode == "I":
+        addrmode = "IMMED"
+    elif addrmode == "Z":
+        addrmode = "ZPAGE"
+    elif addrmode == "ZX":
+        addrmode = "ZPAGE_X"
+    elif addrmode == "A":
+        addrmode = "ABS"
+    elif addrmode == "AX":
+        addrmode = "ABS_X"
+    elif addrmode == "AY":
+        addrmode = "ABS_Y"
+    elif addrmode == "IX":
+        addrmode = "INDIR_X"
+    elif addrmode == "IY":
+        addrmode = "INDIR_Y"
+    elif addrmode == "ACM":
+        addrmode = "ACCUM"
+    elif addrmode == "R":
+        addrmode = "REL"
+    elif addrmode == "IL":
+        addrmode = "IMPL"
+    return addrmode
 
 # taken from https://stackoverflow.com/a/9773142
 def isplit(source, sep=None, regex=False):
@@ -78,26 +81,93 @@ def isplit(source, sep=None, regex=False):
             yield source[start:idx]
             start = idx + sepsize
 
-def next_byte(src: str) -> ByteGenerator:
+def next_byte(line: str, verbose) -> ByteGenerator:
     """
-    Given a UASM source string, returns a generator
+    Given a UASM source string line, returns a generator
     iterating over the corresponding component bytes.
     """
-    idx: int = 0
-    for tok in isplit(src, TOK_DELIMITER):
+    tok_gen = isplit(line, TOK_DELIMITER)
+    head_tok = next(tok_gen)
+
+    spl = head_tok.split(INSTR_DELIMITER)
+    if len(spl) == 1:
+        instr = spl[0].strip()
+        addrmode = None
+    else:
+        # error handling
         try:
-            instr, addrmode = src.split(INSTR_DELIMITER)
+            instr, addrmode = spl
         except ValueError:
-            sys.stderr.write(f"Token: {tok} incorrect format")
+            sys.stderr.write(f"Token: {head_tok} incorrect format\n")
             sys.exit(1)
-        # mnemonics for common addressing modes
-        if addrmode == "I":
-            addrmode = "IMMED"
-        elif addrmode == "Z":
-            addrmode = "ZPAGE"
-        elif addrmode == "ZX":
-            addrmode = "ZPAGE_X"
-        elif addrmode == "A":
-            addrmode = "ABS"
-        
-        
+        if instr not in NES_SPEC:
+            sys.stderr.write(f"Unrecognized instruction {instr}\n")
+            sys.exit(1)
+    
+    # mnemonics for common addressing modes
+    if addrmode is not None:
+        addrmode = translate(addrmode)
+    
+    instr_mapping = NES_SPEC[instr]
+    if addrmode is None and len(instr_mapping) == 1:
+        addrmode = next(iter(instr_mapping.keys()))
+
+    if addrmode not in instr_mapping:
+        sys.stderr.write(f"Unrecognized address mode \"{addrmode}\" "
+                            f"for instruction \"{instr}\"\n")
+        sys.exit(1)
+    
+    head_tok = head_tok.strip()
+    if verbose:
+        print(f"\tToken {head_tok} -> ", end="")
+    
+    yield instr_mapping[addrmode]
+
+    for tok in tok_gen:
+        tok = tok.strip()
+        if verbose:
+            print(f"\tToken {tok} -> ", end="")
+        yield int(tok, base=16)
+
+def fnext_byte(srcfile: io.TextIOWrapper, verbose: bool) -> ByteGenerator:
+    for lineno, line in enumerate(srcfile):
+        if verbose:
+            print(f"Parsing next line: line {lineno}: {line.strip()}")
+        for byte in next_byte(line, verbose):
+            if byte >= 256 or byte < 0:
+                sys.stderr.write("Invalid spec!\n")
+                sys.exit(1)
+            yield byte.to_bytes()
+            
+def assemble(fname: str, out: str, verbose: bool):
+    with open(fname, "r") as content, open(out, "wb") as bytecode:
+        for byte in fnext_byte(content, verbose):
+            if verbose:
+                print(f"byte {hex(int.from_bytes(byte))}")
+            bytecode.write(byte)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description="Assembles a .uasm file into an executable .nes binary.",
+            epilog="Helper script written by me!")
+
+    # verbose mode
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Prints a lot of extra information."
+    )
+
+    # path of .uasm file to assemble
+    parser.add_argument(
+        "filepath", help="Path of the .uasm file to assemble."
+    )
+
+    args = parser.parse_args()
+
+    uasm_file = args.filepath
+    verbose = args.verbose
+
+    o_file = list(os.path.splitext(uasm_file)[:-1])
+    o_file.append(".nes")
+    out_file = "".join(o_file)
+
+    assemble(uasm_file, out_file, verbose)
